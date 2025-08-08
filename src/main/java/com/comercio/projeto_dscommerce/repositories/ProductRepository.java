d4944f7bd8a5aca8cd062df2310671fabb237b58
Samuel Fontes <samuelfontess05@gmail.com>
2025-08-08 20:44:13 -0300

    atualização consulta no database por filtro

--- src/main/java/com/comercio/projeto_dscommerce/repositories/ProductRepository.java
+++ src/main/java/com/comercio/projeto_dscommerce/repositories/ProductRepository.java
@@ -1,8 +1,16 @@
 package com.comercio.projeto_dscommerce.repositories;
 
 import com.comercio.projeto_dscommerce.entities.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
+    @Query("SELECT obj FROM Product obj " +
+            "WHERE UPPER(obj.name) LIKE UPPER(CONCAT('%', :name, '%'))")
+    Page<Product> serachByName(String name, Pageable pageable);
+
+    Page<Product> searchByName(String name);
 }
